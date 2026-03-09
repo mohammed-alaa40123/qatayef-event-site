f8288471ccca3431b6a6ef091368be74d1c19554
mohammed-alaa40123 <mohammedalaa40123@gmail.com>
2026-03-09 09:51:55 +0200

    Deploy latest icons and layout updates

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1198,13 +1198,13 @@ export default function Home() {
             <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5 max-w-6xl mx-auto">
               {[
                 {
-                  name: "IEEE Zewail City SB",
-                  institution: "Zewail City",
+                  name: "IEEE Computational Intelligence Society-Egypt Chapter",
+                  institution: "",
                   country: "Egypt",
                   flag: "🇪🇬",
                   logo: "/images/partners/IEE CIS WHITE.png",
-                  link: "https://www.facebook.com/share/1CB1E47odN/",
-                  type: "IEEE Student Branch",
+                  link: "https://www.facebook.com/ieeebfci/",
+                  type: "IEEE Society Chapter",
                 },
 
                 {
